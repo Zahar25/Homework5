23cee625e7c0fb173be592dd23d91786e5605970
Zahar25 <zpyshny@gmail.com>
2021-12-02 16:07:28 +0200

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,20 @@
+#1. Створіть функцію, всередині якої будуть записано список із п'яти користувачів (ім'я та пароль). Функція повинна приймати три аргументи: два - обов'язкових (<username> та <password>) і третій - необов'язковий параметр <silent> (значення за замовчуванням - <False>).
+class LoginException(Exception):
+    pass
+
+def func (username, password, silent=False):
+    user_info = [
+        {'Vasya': '1214314'}, 
+        {'Kolya': 'wasd2001'}, 
+        {'Lera': '15751'}, 
+        {'Zahar': '080502z'}, 
+        {'Artur': '21212sus'}]
+    for user in user_info:
+        for name, pas in user.items():
+            if username == name and password == pas:
+                return True
+            elif silent:
+                return False
+            else:
+                raise LoginException ("Uncorrect password")
+print(func(username = 'Vasya' , password = '121431', silent = False))
