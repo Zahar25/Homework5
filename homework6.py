83d310238d8f2fab25c01dc7488af72507693fe7
Zahar25 <zpyshny@gmail.com>
2021-12-02 16:08:01 +0200

    Create homework6.py

---
+++ homework6.py
@@ -0,0 +1,14 @@
+def my_range(start, stop, step):
+    while (start<stop):
+        yield start
+        start += step
+
+def main():
+    generator = my_range(0,20,2)
+    try:
+        while True:
+            print(next(generator))
+    except StopIteration:
+        StopIteration
+if __name__ == '__main__':
+    main()
