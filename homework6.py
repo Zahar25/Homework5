200f82fb26bab4d976275e31c3edc1107975b9e4
Zahar Pyshny <zpyshny@gmail.com>
2021-12-06 15:27:07 +0200

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -3,13 +3,16 @@ def my_range(start, stop = None, step = 1):
     if start and not stop and step > 0:
         stop = start
         start = 0
-    while start < stop:
-        yield start
-        start += step
-    while start > stop: 
-        yield start
-        step = -1
-        start += step
+    elif step > 0:
+        while start < stop:
+            yield start
+            start += step
+    elif step < 0:
+        while start > stop:
+            yield start
+            start += step
 
-for i in my_range():
+
+
+for i in my_range(10,0,-1):
     print(i)
